b64300c0ed99ce597af65001f2ca84bb761ae6bb
Arthur Koziel <arthur@arthurkoziel.com>
2010-03-05 16:31:06 +0100

    forgot to change the database_name setting of the testproject

--- testproject/settings.py
+++ testproject/settings.py
@@ -1,5 +1,9 @@
 # Django settings for testproject project.
 
+import os
+
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -10,7 +14,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = '/Users/arthur/Projects/django-slopeone/testproject/db'             # Or path to database file if using sqlite3.
+DATABASE_NAME = os.path.join(PROJECT_PATH, 'db')             # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
